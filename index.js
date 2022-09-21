f24337cb7f97da8f8f834a0bc5165f8f23e246f6
Paul Bosorogan <bosorogan.paul@gmail.com>
2022-09-21 14:50:00 -0400

    commit tree 60365608f04e032dbbb1bbb33274d828bbe3f695

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride <= 400){
+    return 'This one is on me!'
+  }
+  if (ride >=400 && ride <=2000){
+    return 'That will be twenty bucks.'
+  }
+  if (ride >=2000 && ride <=2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  if (ride >=2500){
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let cityName = '';
+  city === 'NYC' ? (cityName = 'Ok, sounds good.') : (cityName ='No go.');
+  return cityName;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let goodbye = '';
+  switch(tip){
+    case 'generous' : goodbye = 'Thank you so much.';
+    break;
+    case 'not as generous' : goodbye = 'Thank you.';
+    break;
+    default : goodbye = 'Bye.';
+  }
+  return goodbye;
 }
